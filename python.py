c04d5adcc64f861c03b95bc362d31d9eb306ff77
Babita Bhatt <109867371+BabitaBhatt2059@users.noreply.github.com>
2023-12-31 13:34:28 +0545

    to print and input in python

---
+++ python.py
@@ -0,0 +1,19 @@
+# print function 
+print("Hello Babita");
+a=10;
+b=30;
+c=a+b;
+print(c);
+# to input
+Name=input("Enter ur name:")
+print(Name)
+# input function is only used to input string not for integers
+A=input("Enter value of A")
+B=input("Enter value of B")
+c=A+B
+print(c)
+# now input string using int()
+A=int(input("enter 1st num"))
+B=int(input("enter 1st num"))
+C=A+B
+print(C)
