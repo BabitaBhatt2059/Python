e8537cb6c6f67e793d3028c21a31ff77e9ab0d0a
Babita Bhatt <babitabhatta37@gmail.com>
2024-01-02 17:53:50 +0545

    program

---
+++ program.py
@@ -0,0 +1,11 @@
+# write a program to add ,sub,multiply and div given no.an
+a=int(input("enter 1st num:"))
+b=int(input("enter 2nd num:"))
+Add=a+b
+sub=a-b
+mult=a*b
+div=a/b
+print(Add)
+print(sub)
+print(mult)
+print(div)
