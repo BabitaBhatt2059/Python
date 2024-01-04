268250a999d8183ca2c95d9ba56bb0634d911285
Babita Bhatt <babitabhatta37@gmail.com>
2024-01-04 11:44:01 +0545

    control statement n loop

--- controlstatement.py
+++ controlstatement.py
@@ -4,4 +4,19 @@ if a%2==0:
     print("the no is even")
 else :
     print ('the no is odd')
-  
+    # program using loop
+    i=1
+    while(i<=10):
+        print(i)
+        i=i+1
+#   write a program to find the sum of 1 to n 
+        n=int(input("enter any no:"))
+        i=1
+        sum=0
+        while(i<=n):
+            sum=sum+i;
+            i=i+1;
+
+print("sum=",sum)
+
+
