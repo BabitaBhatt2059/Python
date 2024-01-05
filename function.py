26018bfe95ae15d63ef5f517330de37d6ab69b94
Babita Bhatt <babitabhatta37@gmail.com>
2024-01-05 13:09:33 +0545

    function with argument and return value

---
+++ function.py
@@ -0,0 +1,24 @@
+def add() :
+  a=int(input("enter value of a:"))
+  b=int(input("enter value of b"))
+  c=a+b
+  print(c)
+add()
+# Function Argument
+def Add(a,b):
+  c=a+b
+  print(c)
+Add(5,6)  
+# Function using argument
+def add(a,b):
+  a=int(input("enter value of x:"))
+  b=int(input("enter value of y:"))
+  c=a+b
+  return(c)
+sum=add(12,13)
+print("sum",sum)
+
+
+
+
+
