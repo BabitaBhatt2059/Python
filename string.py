d5589df8470fc97a8f38f447271a7237ba7d18eb
Babita Bhatt <babitabhatta37@gmail.com>
2024-01-05 23:34:36 +0545

    about string n string operators

---
+++ string.py
@@ -0,0 +1,15 @@
+name="Babita"
+print(name)
+for i in name:
+    print(i)
+    # string operators
+    # concatination
+    x="hello"+"world"
+    print(x)
+    # Replication
+    x=3*"Hello"
+    print(x)
+    # membership
+    "a"in"kapil"
+    "a"is not"kapil"
+# comparision operator
