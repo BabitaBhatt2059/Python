47d20901b5d7eaa6aca0842443bc0dce1a630bf9
Babita Bhatt <babitabhatta37@gmail.com>
2024-01-11 22:27:58 +0545

    list methods in python

--- listmethodinpython.py
+++ listmethodinpython.py
@@ -1,3 +1,11 @@
 a=["ram",10,"shyam",12.5]
 b=len(a)
 print("length of list=",b)
+a=[10,20,30,40,50]
+b=max(a)
+print("max value in the list=",b)
+print("min value in the list=",min(a))
+list1=[2,4,5,6,7]
+list2=[2,4,5,6,7]
+print(Cmp(list1,list2))
+
