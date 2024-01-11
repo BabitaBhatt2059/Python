1f8418a202ed2924a88d1caef50a8cef0c935c3c
Babita Bhatt <babitabhatta37@gmail.com>
2024-01-11 22:31:25 +0545

    list methods in python

--- listmethodinpython.py
+++ listmethodinpython.py
@@ -5,7 +5,14 @@ a=[10,20,30,40,50]
 b=max(a)
 print("max value in the list=",b)
 print("min value in the list=",min(a))
-list1=[2,4,5,6,7]
-list2=[2,4,5,6,7]
-print(Cmp(list1,list2))
+list1 = [2, 4, 5, 6, 7]
+list2 = [2, 4, 5, 6, 7]
 
+result = cmp(list1, list2)
+
+if result == 0:
+    print("Lists are equal")
+elif result < 0:
+    print("List1 is less than List2")
+else:
+    print("List1 is greater than List2")
