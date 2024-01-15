8bfc39f2334942087a033d93bb481db49d1522b3
Babita Bhatt <babitabhatta37@gmail.com>
2024-01-15 21:37:24 +0545

    append n count

---
+++ append.py
@@ -0,0 +1,9 @@
+a=[]
+for i in range(10):
+    x=input("enter tem to add in list")
+    a.append(x)
+    print(a)  
+    # count
+    a=["ram","shyam","hari","gita","ram"]
+    x=a.count("ram")
+    print("frequency=",x)
